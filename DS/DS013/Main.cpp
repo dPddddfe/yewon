038d340444162ad850aedf6053caa03797fa0172
정예원학부생 <ysewon2004@handong.ac.kr>
2025-03-27 21:37:57 +0900

    main

--- DS/DS013/Main.cpp
+++ DS/DS013/Main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 struct Cafe{
     string cafename;
     int manunum;
-    int* rist;
+    string* rist;
 };
 
 struct Menu{
@@ -21,28 +21,32 @@ int main(){
 
     Cafe* r1 = new Cafe;
     Menu* m1 = new Menu;
-    [r1->]
 
     addCafe(*r1);
     cin >> r1->manunum;
+
+    r1->rist = new string[r1->manunum];
+    
     for(int i =0; i<r1->manunum; i++){
         addMenu(*m1);
-        r1->rist ++;
+        r1->rist[i] = m1->manuname + " " + to_string(m1->money);
     }
-    for(int i =0; i<r1->manunum; i++){
-        displayManu(*r1);
-        r1->rist ++;
-    }
+    
+    displayManu(*r1);
     return 0;
 }
 
 void addCafe(Cafe &r1){
-    getline(cin,r1.cafename) ;
+    getline(cin,r1.cafename);
 }
 void addMenu(Menu &m1){
     cin >> m1.manuname >> m1.money;
+
 }
 void displayManu(Cafe &r1){
-    printf("===== %s =====\n",r1.cafename);
-    printf("%s %d",r1.manunum, )
+    printf("===== %s =====\n",r1.cafename.c_str());
+    for(int i=0; i<r1.manunum; i++){
+        printf("%s\n",r1.rist[i].c_str());
+    }
+    printf("=================\n");
 }
